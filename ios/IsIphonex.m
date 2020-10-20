9e6184885ea2b3206d5ade7c8a00987fd4df4c96
rickL <511189918@qq.com>
2020-10-20 14:41:33 +0800

    ios isIphonex methoud

--- ios/IsIphonex.m
+++ ios/IsIphonex.m
@@ -3,12 +3,30 @@
 
 @implementation IsIphonex
 
+if (@available(iOS 11.0, *)) {
+    CGFloat a =  [[UIApplication sharedApplication] delegate].window.safeAreaInsets.bottom;
+    NSLog(@"%f",a);
+} else {
+    // Fallback on earlier versions
+}
+
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
 {
     // TODO: Implement some actually useful functionality
     callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+}
+
+RCT_REMAP_METHOD(isIphonex,
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    BOOL isPhoneX = NO;
+    if (@available(iOS 11.0, *)) {
+        isPhoneX = [[UIApplication sharedApplication] delegate].window.safeAreaInsets.bottom > 0.0;
+    }
+    resolve(isPhoneX);
 }
 
 @end
